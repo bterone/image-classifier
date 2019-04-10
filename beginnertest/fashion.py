7b16ffe6ee8178fd7d5b40ab821be96abe663d9a
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-04-10 17:56:11 +0530

    First commit

---
+++ beginnertest/fashion.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
