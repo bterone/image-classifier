e939e8eff77b5efa2cc78a8e04a23da4253f9524
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-04-11 21:45:36 +0530

    Mapped out text to index for shakespear text

---
+++ RNNs/textgeneration.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+print('Length of text: {} characters'.format(len(text)))
+
+vocab = sorted(set(text))
+print('{} unique characters'.format(len(vocab)))
+
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
