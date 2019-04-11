dafdccef7e2dad78536072309edc5928c01c989d
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-04-11 14:45:55 +0530

    Shows clothing w/ different classifications

--- beginnertest/fashion.py
+++ beginnertest/fashion.py
@@ -45,3 +45,55 @@ test_loss, test_acc = model.evaluate(test_images, test_labels)
 # Testing accuracy = 0.8744
 # Represents overfitting when model (When model performs worse on new, unseen data)
 print('\nTest accuracy: ', test_acc)
+
+predictions = model.predict(test_images)
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+    
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                    100*np.max(predictions_array),
+                                    class_names[true_label]),
+                                    color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color='#777777')
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+"""i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(1, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+plt.show()"""
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows * num_cols
+plt.figure(figsize= (2*2*num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
