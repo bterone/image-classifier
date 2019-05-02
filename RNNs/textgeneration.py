99e78fda56a81e63118089d91b93d9ee1996435f
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-05-02 07:17:49 +0700

    Split text to input data vs target data

--- RNNs/textgeneration.py
+++ RNNs/textgeneration.py
@@ -19,3 +19,36 @@ idx2char = np.array(vocab)
 
 text_as_int = np.array([char2idx[c] for c in text])
 
+#print('{')
+#for char, _ in zip(char2idx, range(20)):
+#    print(' {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+#print(' ... \n}')
+
+#print('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+# Maximum length sentence for single input
+seq_length = 100
+examples_per_epoch = len(text)//seq_length
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+#for i in char_dataset.take(5):
+#  print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
+#for item in sequences.take(5):
+#    print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]  # Everything except the last item
+    target_text = chunk[1:]  # Starts from the second item
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print('Target data: ', repr(''.join(idx2char[target_example.numpy()])))
+
