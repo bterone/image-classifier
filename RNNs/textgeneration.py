dd51c48dbbe238b5125ce78ae03ec75153eac386
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-05-04 11:38:26 +0700

    Creating checkpoints

--- RNNs/textgeneration.py
+++ RNNs/textgeneration.py
@@ -126,3 +126,25 @@ model.compile(optimizier='adam', loss=loss)
 
 # Configuring checkpoints
 
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt_{epoch}')
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True
+)
+
+EPOCHS = 10
+
+history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+
+# GENERATE TEXT
+# Restore latest checkpoint
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+#model.summary()
